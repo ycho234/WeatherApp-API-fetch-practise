f89ec2ed19265639899c3c5a49219618e1e715e7
maramar0414 <maramar0414@gmail.com>
2023-09-22 17:02:37 +0100

    final

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 //Create a function to get and display the weather data
 async function getWeather() {
+
+    //fetch the weather data
   const response = await fetch(
     "https://api.open-meteo.com/v1/forecast?latitude=51.5085&longitude=-0.1257&current_weather=true",
     {
@@ -8,22 +10,28 @@ async function getWeather() {
       },
     }
   );
+
+  //Check if the function is working
   console.log("it's working");
 
+  //if the request fail log an error
   if (!response.ok) {
     console.error(`Status: ${response.status}`);
     console.error(`Text: ${await response.text()}`);
     return;
   }
 
+//weather object 
   const data = await response.json();
   console.log(typeof data, data);
-  // above code will take data and change the temperature//
-
+  
+//Temperature display
   const tempLondon = document.getElementById("temperatureText");
   const temperature = data.current_weather.temperature;
   tempLondon.textContent = temperature;
   console.log(data.current_weather.temperature);
+
+ //Time display 
 
   const timeLondon = document.getElementById("timeText");
   const time = data.current_weather.time;
@@ -32,7 +40,7 @@ async function getWeather() {
 }
 getWeather();
 
-// making button work
+// Button
 const button = document.getElementById("weatherButton");
 button.addEventListener("click", function () {
   window.location.href = "https://www.bbc.com/weather/2643743";
