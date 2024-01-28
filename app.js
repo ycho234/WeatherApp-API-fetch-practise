4971c7818e1fcdda4e97a2f69956f3a6f91da37c
yusra-aina choudhury <yusrachoudhury123@gmail.com>
2024-01-28 20:52:32 +0000

    weather

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 //Create a function to get and display the weather data
 async function getWeather() {
-
-    //fetch the weather data
+  //fetch the weather data
   const response = await fetch(
     "https://api.open-meteo.com/v1/forecast?latitude=51.5085&longitude=-0.1257&current_weather=true",
     {
@@ -21,29 +20,30 @@ async function getWeather() {
     return;
   }
 
-//weather object 
+  //weather object
   const data = await response.json();
   console.log(typeof data, data);
 
-//Temperature display
+  //Temperature display
   const tempLondon = document.getElementById("temperatureText");
   const temperature = data.current_weather.temperature;
   tempLondon.textContent = temperature;
   console.log(data.current_weather.temperature);
 
- //Time display 
+  //Time display
 
   const timeLondon = document.getElementById("timeText");
   const time = data.current_weather.time;
   timeLondon.textContent = time;
   console.log(data.current_weather.time);
-}
-getWeather();
 
-// Button
-const button = document.getElementById("weatherButton");
-button.addEventListener("click", function () {
-  window.location.href = "https://www.bbc.com/weather/2643743";
-});
+  // units display
+
+  const unitsLondon = document.getElementById("unitText");
+  const units = data.current_weather_units.temperature;
+  unitsLondon.textContent = units;
+}
+
+getWeather();
 
 console.log(document.addEventListener("DOMContentLoaded", getWeather));
